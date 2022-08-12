2e4e5f1081e08b78ce6ea7f6d57f93a3bbdf6229
Oleksii Matviichuk <77227774+AlexDvor@users.noreply.github.com>
2022-08-12 20:29:38 +0200

    save

--- controllers/cv/getCv.js
+++ controllers/cv/getCv.js
@@ -1,13 +1,21 @@
 const getCV = async (req, res, next) => {
-  const path = require("path");
-  const file = path.join(__dirname, "../../", "data", "cv.pdf");
-  res.download(file, function (err) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Success");
-    }
-  });
+  try {
+    const path = require("path");
+    const file = path.join(__dirname, "../../", "data", "cv.pdf");
+    res.download(file);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = getCV;
+
+// const path = require("path");
+//   const file = path.join(__dirname, "../../", "data", "cv.pdf");
+//   res.download(file, function (err) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("Success");
+//     }
+//   });
